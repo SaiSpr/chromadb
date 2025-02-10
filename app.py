fe115e3569998fa303c69fa688ca81f9ff83f738
Sai Prakash <63905195+SaiSpr@users.noreply.github.com>
2025-02-10 16:08:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+import pysqlite3 as sqlite3
+
 import streamlit as st
 import pandas as pd
 import chromadb
 import torch
 from sentence_transformers import SentenceTransformer
 from gradio_client import Client
-__import__('pysqlite3')
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
+
 
 # -------------------------------
 # ✅ Initialize Hugging Face Client
